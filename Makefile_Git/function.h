37ae8596dbe3be1dd8b73b76f4d4b657fd6216d7
baramgebi <changup.yi@gmail.com>
2021-03-14 23:42:08 +0100

    upload

---
+++ Makefile_Git/function.h
@@ -0,0 +1,6 @@
+#ifndef function_h
+#define function_h
+
+int function();
+
+#endif
---
+++ Makefile_Git/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include "function.h" 
+
+int main(int argc, char** argv)
+{
+	int i;
+
+	i=function();
+	printf("Main Beispiel %d\n",i);
+
+	return 0;
+}
+
